6f74f52ed65fd509d58ef55d526aca48f0399caf
arafat5549 <arafat5549@gmail.com>
2017-07-08 10:41:46 +0800

    log

---
+++ W1734/src/socket/EchoClient.java
@@ -0,0 +1,83 @@
+package socket;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClient implements Runnable{
+
+	private int port;
+	private String host;
+	public EchoClient(String host,int port){
+		this.port = port;
+		this.host = host;
+	}
+	
+	public static void main(String[] args) {
+		
+		//1.IP地址#本地Ip地址 127.0.0.1 等价于 localhost
+		//localhost最后会被转化为127.0.0.1 本地的DNS配置
+		//C:\Windows\System32\drivers\etc\hosts里面保存你们的本地DNS配置
+		
+		//CDN
+		
+		//2.端口
+		EchoClient ec = new EchoClient("127.0.0.1",8091);
+		Thread t = new Thread(ec);
+		t.start();
+	}
+
+	@Override
+	public void run() {
+		 Socket socket = null;
+		 PrintWriter out = null;
+		 BufferedReader in = null;
+		try {
+			socket = new Socket(host, port);
+			out = new PrintWriter(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			if(in!=null){
+                try{
+                    in.close();
+                }
+                catch (IOException el){
+                    el.printStackTrace();
+                }
+            }
+
+            if(out!=null){
+                out.close();
+                out=null;
+            }
+
+            if(socket != null){
+                try{
+                    socket.close();
+                }catch (IOException el){
+                    el.printStackTrace();
+                }
+                socket=null;
+            }
+		}
+		
+	}
+	
+	
+}
---
+++ W1734/src/socket/EchoServer.java
@@ -0,0 +1,119 @@
+package socket;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class EchoServer implements Runnable {
+
+	@Override
+	public void run() {
+		ServerSocket server = null;
+		Socket socket = null;
+		try {
+			server = new ServerSocket(port);//1.服务器建立监听端口
+			System.out.println("Server Start in port:"+port);
+			while(true){
+				socket = server.accept();//2.建立Socket连接
+				System.out.println("--------");
+					//3.处理数据流 - 
+				
+			}
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+//	
+//	public static void main(String[] args) {
+//		
+//		int port = 8091;
+//		
+//		ServerSocket server = null;
+//		Socket socket = null;
+//		try {
+//			server = new ServerSocket(port);//1.服务器建立监听端口
+//			System.out.println("Server Start in port:"+port);
+//			while(true){
+//				socket = server.accept();//2.建立Socket连接
+//				System.out.println("--------");
+//				//3.处理数据流 - 
+//				new Thread(new TimerServerHandler(socket)).start();
+//			}
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//
+//
+//}
+//
+//
+//class TimerServerHandler implements Runnable{
+//	Socket socket =null;
+//	public TimerServerHandler(Socket socket){
+//		this.socket = socket;
+//	}
+//	//
+//	@Override
+//	public void run() {
+//		BufferedReader in = null;  //客户端给我发的信息
+//		PrintWriter out = null;    //我给客户端发的信息
+//		try {
+//			 out = new PrintWriter(socket.getOutputStream());
+//			 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//			 
+//			 String currentTime = null;
+//			 String body = null;
+//			 //while(true) 
+//			 {
+//	                body = in.readLine();
+//	                System.out.println("body="+body);
+//	               // if (body == null) break;
+//	                System.out.println("recvive order : " + body);
+//
+//	                currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+//	                        System.currentTimeMillis()).toString() : "BAD ORDER";
+//	                
+//	                out.println(currentTime);
+//	            }
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}finally{
+//			 if(in!=null){
+//	                try{
+//	                    in.close();
+//	                }
+//	                catch (IOException el){
+//	                    el.printStackTrace();
+//	                }
+//	            }
+//
+//	            if(out!=null){
+//	                out.close();
+//	                out=null;
+//	            }
+//
+//	            if(this.socket != null){
+//	                try{
+//	                    this.socket.close();
+//	                }catch (IOException el){
+//	                    el.printStackTrace();
+//	                }
+//	                this.socket=null;
+//	            }
+//	        }
+//			
+//	}
+//}
